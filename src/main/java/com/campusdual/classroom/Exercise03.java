ea5e9a095d9a5249b761431cb5b3850d9ca0d9e0
SilviaGarciaBouza <silvia.gar.bou.dev@gmail.com>
2025-05-22 09:40:16 +0200

    Ejercicio 3 completado

--- src/main/java/com/campusdual/classroom/Exercise03.java
+++ src/main/java/com/campusdual/classroom/Exercise03.java
@@ -1,27 +1,47 @@
 package com.campusdual.classroom;
 
+import java.util.Scanner;
+
 public class Exercise03 {
 
     // Esto es una funcion, ya que devuelve un valor
-    // TODO ↓: Tiene que devolver el siguiente mensaje: "Hola. Bienvenido " y la cadena de textos que se le pasa por parámetro.
+    // TODO ↓: Tiene que devolver el siguiente mensaje: 
+    //  "Hola. Bienvenido " y la cadena de textos que se le pasa por parámetro.
     public static String greetings(String name) {
-        return null;
+          return "Hola. Bienvenido " + name;
     }
 
     // Esto es una funcion, ya que devuelve un valor
-    // TODO ↓: Tiene que devolver el siguiente mensaje: "Ups. No pudimos validar tus datos. Tu nombre de usuario no es " y la cadena de texto que se le pasa por parámetro
+    // TODO ↓: Tiene que devolver el siguiente mensaje:
+    //  "Ups. No pudimos validar tus datos. Tu nombre de usuario no es "
+    //  y la cadena de texto que se le pasa por parámetro
     public static String error(String nombre) {
-        return null;
+        return "Ups. No pudimos validar tus datos. Tu nombre de usuario no es " + nombre;
+
     }
 
     //Esto es un procedimiento, ya que no devuelve un valor
-    // TODO ↓: Tiene que comprobar si el usuario que recibe por parámetro es "Sebas" y su contraseña es "sebas01". Si es correcto, que muestre el mensaje de la función greetings() y si no que muestre el mensaje de la función error()
+    // TODO ↓: Tiene que comprobar si el usuario que recibe por parámetro es
+    //  "Sebas" y su contraseña es "sebas01".
+    //  Si es correcto, que muestre el mensaje de la función greetings()
+    //  y si no que muestre el mensaje de la función error()
     public static void checkUser(String user, String pass) {
-
+        String validUser = "Sebas";
+        if (user.equals("Sebas") && pass.equals("sebas01")){
+            System.out.println(greetings(user));
+        }else {
+            System.out.println(error(validUser));
+        }
     }
 
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Escribe tu nombre: ");
+        String user= scanner.nextLine();
+        System.out.println("Escribe tu contraseña: ");
+        String pass= scanner.nextLine();
+        checkUser(user, pass);
     }
 }
